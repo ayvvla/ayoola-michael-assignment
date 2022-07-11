e34c4d70d7cf58f75d0db6e29a5f959974c05ecb
Ayoola Michael <65915666+ayvvla@users.noreply.github.com>
2022-07-11 23:19:28 +0000

    return array.length instead of just the array

--- src/assignment.js
+++ src/assignment.js
@@ -14,9 +14,14 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  let sum = 0;
+  
+  for (let i = 0 ; i < arrayOfNumbers.length ; i++) {
+    sum += arrayOfNumbers[i];
+  }
+    return sum;
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -27,10 +32,17 @@ function sumOfNumbers(arrayOfNumbers) {
  * @param {Array} arrayOfNumbers the array containing even or non-even numbers
  * @returns number the count of even numbers
  */
-function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+function countEvenNumbers(arrayOfNumbers2) {
+  let evenArray = []
+
+  for (x = 0 ; x < arrayOfNumbers2.length ; x++){
+    if (arrayOfNumbers2[x] % 2 == 0) {
+      evenArray.push(arrayOfNumbers2[x])
+    }
+  }
+  return evenArray.length
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
